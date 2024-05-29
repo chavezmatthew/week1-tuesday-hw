5578d2157b9f716baed0e137da6794d7e926b21d
chavezmatthew <chavezmatthew21@gmail.com>
2024-05-28 21:37:50 -0500

    Tuesday homework

---
+++ 4.py
@@ -0,0 +1,25 @@
+# Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, 
+# given their individual prices. For example, what would be the cost of bread, peanut butter, and jelly be? 
+# Prices don't need to be realistic
+
+
+bread_price = 23
+peanut_butter_price = 33
+jelly_price = 43
+
+total_cost = (bread_price + peanut_butter_price + jelly_price)
+
+print (total_cost)
+
+
+
+# Task 2: Bank Interest (Extra Credit) If you have a savings account with a particular initial amount and a 
+# fixed yearly interest rate, calculate the total amount in your account after a year. 
+# So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. 
+# For the example the expected outcome would be $10,700.
+
+initial_amount = 10000
+interest_rate = 0.07
+total_amount = initial_amount * (1+interest_rate)
+
+print (total_amount)
---
+++ 5.py
@@ -0,0 +1,18 @@
+# Task 1: Validating Calculations Calculate the value of a particular arithmetic expression twice: 
+# once normally, and once using parentheses. Compare the two results to see if they match.
+
+five_plus_four = 5+4
+five_plus_four_parens = (5+4)
+
+print (five_plus_four)
+print (five_plus_four_parens)
+
+
+# Task 2: Mix and Match Combine arithmetic (+), logical (or), and comparison (>) operators in a 
+# single expression and predict the outcome. Then, compute the expression to see if the prediction was correct.
+
+
+mix_match = (5+4) > 3 or (6+9) > 3
+#true
+
+print (mix_match)
